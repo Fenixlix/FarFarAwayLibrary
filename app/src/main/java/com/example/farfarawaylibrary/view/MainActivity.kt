50b1e84c34030224aee6ce87785bd61f13f98066
Felix Escandon <96452288+Fenixlix@users.noreply.github.com>
2022-04-16 17:38:13 -0500

    First Commit, Mvvm structure

---
+++ app/src/main/java/com/example/farfarawaylibrary/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.farfarawaylibrary.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.farfarawaylibrary.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
